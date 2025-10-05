d9befccb4f8e15c3b43b5569db9fd070606334f2
sam-ludlow <sam@spludlow.co.uk>
2025-10-05 11:29:32 +0100

    wetwe

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.14")]
-[assembly: AssemblyFileVersion("1.14")]
+[assembly: AssemblyVersion("1.15")]
+[assembly: AssemblyFileVersion("1.15")]
