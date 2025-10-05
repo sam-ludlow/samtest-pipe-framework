f43de2bf1ac355492728fa86d16fed6391c16b7e
sam-ludlow <sam@spludlow.co.uk>
2025-10-05 12:03:58 +0100

    23r123r32

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.17")]
-[assembly: AssemblyFileVersion("1.17")]
+[assembly: AssemblyVersion("1.18")]
+[assembly: AssemblyFileVersion("1.18")]
